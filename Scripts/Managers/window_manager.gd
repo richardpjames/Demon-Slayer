fa2aaf7331d51f03ea01dc7d9f52ba9e97ea1e2c
Richard James <richard@richardpjames.com>
2024-09-02 10:46:17 +0100

    Added key for full screen

---
+++ Scripts/Managers/window_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Store whether we are full screen or not
+var _full_screen: bool
+
+# Check for input to switch full screen
+func _process(delta: float) -> void:
+	if(Input.is_action_just_pressed("FullScreen")):
+		_full_screen = !_full_screen
+		_enter_exit_full_screen()
+
+# To actually enter or exit full screen mode
+func _enter_exit_full_screen() -> void:
+	if(_full_screen):
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
